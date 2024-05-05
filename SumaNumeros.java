886a9fc2c04f4b9e0a0a320629e146c5003e2877
josutrres <josutrres@gmail.com>
2024-05-04 23:13:14 -0400

    new number feature

--- SumaNumeros.java
+++ SumaNumeros.java
@@ -3,8 +3,9 @@ public class SumaNumeros{
     public static void main(String[] args){
         int n1 = 1;
         int n2 = 3;
+        int n3 = 4;
         int resultado;
-        resultado = n1 + n2;
-        System.out.println(n1 + " + " + n2 + " = " + resultado)
+        resultado = n1 + n2 + n3;
+        System.out.println(n1 + " + " + n2 + " + " + n3 + " = " + resultado);
     }
 }
