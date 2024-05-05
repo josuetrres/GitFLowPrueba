13baaf51fb6b76a951f2fc59dd9f167e7c2a5fbe
josutrres <josutrres@gmail.com>
2024-05-04 22:33:25 -0400

    first commit

---
+++ SumaNumeros.java
@@ -0,0 +1,10 @@
+
+public class SumaNumeros{
+    public static void main(String[] args){
+        int n1 = 1;
+        int n2 = 3;
+        int resultado;
+        resultado = n1 + n2;
+        System.out.println(n1 + " + " + n2 + " = " + resultado)
+    }
+}
